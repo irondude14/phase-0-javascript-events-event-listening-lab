3d4c67c03d171a91a6bcc6784852110c99d303db
Vladyslav <vkadyrov@protonmail.com>
2022-10-08 13:38:53 -0400

    commit tree 7779ed6211eb27cb2607dcbe96c08044b790eb6a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('I was cliked!');
+    };
+
+    input.addEventListener('click', clickAlert);
+};
+
+addingEventListener();
